cedda28f7332dfd11f96a21c1f8919afc4dc54be
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-06-17 13:24:31 +0530

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-
+import Link from "next/link";
 
 
 const Nav = () => {
   return (
-        
-
-      </Link>
+        <
       <Link href='/posts' className='black_btn'>All Posts</Link>
 
           <div><Link href='/about' className='black_btn'>
