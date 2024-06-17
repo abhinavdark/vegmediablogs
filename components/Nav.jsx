2d65162422d5ac0d403b4c04d0cefef57d6df607
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-06-17 13:44:33 +0530

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,16 +3,21 @@ import Link from 'next/link';
 const Nav = () => {
   return (
     <nav>
-      <Link href='/posts' className='black_btn'>
-        All Posts
-      </Link>
-      <div>
-        <Link href='/about' className='black_btn'>
-          About
-        </Link>
-      </div>
+      <ul>
+        <li>
+          <Link href='/posts' className='black_btn'>
+            All Posts
+          </Link>
+        </li>
+        <li>
+          <Link href='https://calendly.com/vegamediain/30min' className='black_btn'>
+            Let's Talk
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
 
 export default Nav;
+
