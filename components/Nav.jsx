65e539712a96ee375890c45adfb1a3b0d6968c2e
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-06-17 13:23:19 +0530

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
+
+
 const Nav = () => {
   return (
-        <nav className='flex-between gap-x-1 w-full mb-2 mt-4 max-sm:gap-x-0 min-w-[360px]'>
-        <Link href='/' className='flex gap-2 flex-center pl-1 max-sm:gap-0'>
-    
+        
+
       </Link>
       <Link href='/posts' className='black_btn'>All Posts</Link>
 
