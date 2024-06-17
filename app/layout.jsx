3eeaf02f1b698cbd90d9ed2a90a3afe3471d3596
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-06-17 13:08:32 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,8 +3,11 @@ import Nav from 'components/Nav';
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata ={
-    title:"Devcut",
-    description:"A Blog for developers,cybersecurity enthusiaths & linux users",
+    title:"Vegamedia",
+    description:"A Blog for realestate,yatchers,winery",
+    keywords :"digital marketing, digital marketing agency, digital marketing, real estate luxury, jet charter marketing india, online advertising, SEo optimizationyatch networking mission missionsails cruisesrentals trips luxury packages campaigns solbenef fans hashtags scripts ping listing hotel weekend catering services amen,digital marketing salary, what is digital marketing, is digital marketing, digital marketing company, digital marketing jobs, digital marketing services, digital marketing course, power digital marketing, digital marketing strategy, digital marketing certificate, freelance digital marketing,vegamedia,Vegamedia,VegaMedia",
+
+
 }
 
 
