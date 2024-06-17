b62541b7a197b97c1e9b5385c8cf53afb7c117e1
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-06-17 13:15:47 +0530

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,14 @@ const Nav = () => {
   return (
         <nav className='flex-between gap-x-1 w-full mb-2 mt-4 max-sm:gap-x-0 min-w-[360px]'>
         <Link href='/' className='flex gap-2 flex-center pl-1 max-sm:gap-0'>
-        
+        <Image
+          src='/assets/logo-no-background.svg'
+          alt='logo'
+          width={90}
+          height={50}
+          className='object-contain'
+        />
+          
       </Link>
       <Link href='/posts' className='black_btn'>All Posts</Link>
       
